ec5cb38439dd62d56886aaec5396faf88c07eecc
Anthony Bucci <lastralla@hotmail.com>
2013-04-22 12:24:04 -0300

    Update jquery.hasAttr.js

--- jquery.hasAttr.js
+++ jquery.hasAttr.js
@@ -7,6 +7,8 @@
   $.fn.hasAttr = function (attrName) {
 		return (this.filter(function() {
 			if ( typeof $(this).attr(attrName) !== undefined ) {
+				// note: this test does not distinguish between an element with no 'attrName' attribute
+				// or an element with an 'attrName' equal to an empty string, both cases return false
 				return true;
 			}
 			return false;
